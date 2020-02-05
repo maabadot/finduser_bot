33e3aeb68fdd14cb98c8d1794795af2d5a84fa5c
Sergey Bolshakov <spooky5ya@yandex.ru>
2020-02-05 23:27:26 +0300

    Shit is working

--- index.js
+++ index.js
@@ -3,36 +3,56 @@ const config = require('./config.json');
 const request = require('request');
 
 function checkVK(nickname, socNet) {
-    request.get(`http://vk.com/${nickname}`).on('response', function(response) {
-        if (response && response.statusCode === 200) {
-            socNet.VK = `http://vk.com/${nickname}`; // 200
-        }
-        console.log('VK checked... ' + nickname)
+    return new Promise((resolve) => {
+        request.get(`http://vk.com/${nickname}`).on('response', function(response) {
+            if (response && response.statusCode === 200) {
+                socNet.VK = `http://vk.com/${nickname}`; // 200
+            }
+            console.log('VK checked... ' + nickname);
+            resolve();
+        });
     });
+
 }
 
 function checkIG(nickname, socNet) {
-    request.get(`http://instagram.com/${nickname}`).on('response', function(response) {
-        if (response && response.statusCode === 200) {
-            socNet.IG = `http://instagram.com/${nickname}`; // 200
-        }
-        console.log('IG checked... ' + nickname)
+    return new Promise((resolve) => {
+        request.get(`http://instagram.com/${nickname}`).on('response', function(response) {
+            if (response && response.statusCode === 200) {
+                socNet.IG = `http://instagram.com/${nickname}`; // 200
+            }
+            console.log('IG checked... ' + nickname);
+            resolve();
+        });
     });
+
 }
 
 function checkTW(nickname, socNet) {
-    request.get(`http://twitter.com/${nickname}`).on('response', function(response) {
-        if (response && response.statusCode === 200) {
-            socNet.TW = `http://twitter.com/${nickname}`; // 200
-        }
-        console.log('TW checked... ' + nickname)
+    return new Promise((resolve) => {
+        request.get(`http://twitter.com/${nickname}`).on('response', function(response) {
+            if (response && response.statusCode === 200) {
+                socNet.TW = `http://twitter.com/${nickname}`; // 200
+            }
+            console.log('TW checked... ' + nickname);
+            resolve();
+        });
     });
+
 }
 
-function checkAll(nickname, socNet) {
-    checkVK(nickname, socNet);
-    checkIG(nickname, socNet);
-    checkTW(nickname, socNet);
+function answer(ctx, socNet) {
+    ctx.reply(`${socNet.VK}\n${socNet.IG}\n${socNet.TW}`);
+}
+
+async function checkAll(nickname, socNet, ctx) {
+    Promise.all([
+        checkVK(nickname, socNet),
+        checkIG(nickname, socNet),
+        checkTW(nickname, socNet)])
+        .then(() => {
+            answer(ctx, socNet);
+        });
 }
 
 const bot = new Telegraf(config.token);
@@ -44,10 +64,13 @@ bot.on('message', function(ctx) {
         IG: 'Not found',
         TW: 'Not found'
     };
-    checkAll(ctx.message.text, socialNetworks);
-    ctx.reply(`${socialNetworks.VK}\n${socialNetworks.IG}\n${socialNetworks.TW}`);
+    checkAll(ctx.message.text, socialNetworks, ctx);
+
+
+    //answer(ctx, socialNetworks);
+
     // setTimeout(() => {
-    //     ctx.reply(`${socialNetworks.VK}\n${socialNetworks.IG}\n${socialNetworks.TW}`);
+    //     answer(ctx, socialNetworks);
     // },3000);
 });
 
