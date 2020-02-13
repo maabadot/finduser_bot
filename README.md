b7f9c52baa34998c3d584dc8107af04218de5773
Sergey Bolshakov <spooky5ya@yandex.ru>
2020-02-13 19:47:24 +0300

    Added Instagram bio parsing for external links and input validation

--- README.md
+++ README.md
@@ -8,7 +8,13 @@ NPM dependencies:
 $ npm i
 ```
 
-To make this work, you need to add **config.json** file to root folder. There you should place your bot token like this { "token": "YOUR_TOKEN" }.
+To make this work, you need to add **config.json** file to root folder. There you should place your bot token like this:
+
+```sh
+{ 
+    "token": "YOUR_TOKEN" 
+}
+```
 
 [Telegraf]: <https://www.npmjs.com/package/telegraf>
 [Request]: <https://www.npmjs.com/package/request>
--- index.js
+++ index.js
@@ -6,9 +6,9 @@ function checkVK(nickname, socNet) {
     return new Promise((resolve) => {
         request.get(`http://vk.com/${nickname}`).on('response', function(response) {
             if (response && response.statusCode === 200) {
-                socNet.VK = `http://vk.com/${nickname}`; // 200
+                socNet.VK = `VK: http://vk.com/${nickname}`; // 200
             }
-            console.log('VK checked... ' + nickname);
+            console.log(`VK checked... ${nickname} ${response && response.statusCode}`);
             resolve();
         });
     });
@@ -17,32 +17,47 @@ function checkVK(nickname, socNet) {
 
 function checkIG(nickname, socNet) {
     return new Promise((resolve) => {
-        request.get(`http://instagram.com/${nickname}`).on('response', function(response) {
+        request.get(`http://instagram.com/${nickname}`, function (error, response, body) {
             if (response && response.statusCode === 200) {
-                socNet.IG = `http://instagram.com/${nickname}`; // 200
+                socNet.IG = `IG: http://instagram.com/${nickname}`; // 200
+                let first = body.indexOf('external_url');
+                let last = body.lastIndexOf('external_url');
+                let slice = body.slice(first, last);
+                console.log(first, last);
+                console.log(slice);
+                if (slice != 'external_url":null,"') {
+                    slice = slice.slice(15, -3);
+                    socNet.links.push(slice);
+                }
             }
-            console.log('IG checked... ' + nickname);
+            console.log(`IG checked... ${nickname} ${response && response.statusCode}`);
             resolve();
-        });
+        })
     });
-
 }
 
 function checkTW(nickname, socNet) {
     return new Promise((resolve) => {
         request.get(`http://twitter.com/${nickname}`).on('response', function(response) {
             if (response && response.statusCode === 200) {
-                socNet.TW = `http://twitter.com/${nickname}`; // 200
+                socNet.TW = `TW: http://twitter.com/${nickname}`; // 200
             }
-            console.log('TW checked... ' + nickname);
+            console.log(`TW checked... ${nickname} ${response && response.statusCode}`);
             resolve();
         });
     });
-
 }
 
 function answer(ctx, socNet) {
-    ctx.reply(`${socNet.VK}\n${socNet.IG}\n${socNet.TW}`);
+    let ans = `${socNet.VK}\n${socNet.IG}\n${socNet.TW}`;
+    if (socNet.links.length != 0) {
+        ans += `\n\nFollowing external links were found:\n`
+        for (let i = 0; i < socNet.links.length; i++) {
+            ans += `${socNet.links[i]}\n`
+        }
+    }
+    console.log(socNet.links);
+    ctx.reply(ans);
 }
 
 async function checkAll(nickname, socNet, ctx) {
@@ -60,14 +75,19 @@ bot.start((ctx) => ctx.reply('Welcome'));
 bot.help((ctx) => ctx.reply('Help phrase'));
 bot.on('message', function(ctx) {
     let socialNetworks = {
-        VK: 'Not found',
-        IG: 'Not found',
-        TW: 'Not found'
+        VK: 'VK: Not found',
+        IG: 'IG: Not found',
+        TW: 'TW: Not found',
+        links: []
     };
-    checkAll(ctx.message.text, socialNetworks, ctx);
+    let regex = /^[a-zA-Z0-9_\-:?%!@"#&№$;\^*()]+$/;
+    if (regex.test(ctx.message.text)) {
+        checkAll(ctx.message.text, socialNetworks, ctx);
+    }
+    else {
+        ctx.reply('Your message seems to have non-latin symbols.');
+    }
 });
 
 
-bot.launch(function () {
-    console.log('Bot is working...');
-});
+bot.launch();
